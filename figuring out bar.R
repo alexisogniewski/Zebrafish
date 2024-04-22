2c6111e6d8d548458b138e154eac5d1ea3db2d36
Alex Ogniewski <is7017yq@mnstate.edu>
2024-04-22 08:05:50 -0600

    finished graph for presentation

--- figuring out bar.R
+++ figuring out bar.R
@@ -6,8 +6,10 @@ library(readxl)
 zebrafishy <- read_excel("zebrafish_test (1).xlsx")
 
 ##creating a bar graph
-ggplot(zebrafishy,aes(x = prefer_agressive))+
+ggplot(zebrafishy,aes(x = prefer_agressive, fill = prefer_agressive))+
   geom_bar(stat = "count")+
+  scale_fill_manual(values = c("deepskyblue","firebrick"))+
   labs(title = "Preference of Female Zebrafish", 
        x = "Preferred Agressive Male", 
-       y = "Number of Fish")
+       y = "Number of Fish")+
+  theme(legend.position = "none")
