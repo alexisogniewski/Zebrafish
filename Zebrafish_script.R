3a3b1e47d49af7c755b4ce4c0547a9d1e1fcb0c8
Alex Ogniewski <is7017yq@mnstate.edu>
2024-04-30 14:22:29 -0600

    Got the graph with the REAL data

--- Zebrafish_script.R
+++ Zebrafish_script.R
@@ -4,4 +4,15 @@ library(ggplot2)
 library(readxl)
 
 ## Loading in data
+zebra <- read_excel("Zebrafish.xlsx", na = c("NA",""))
 
+#visualizing the data
+ggplot(zebra, aes(x = prefer, fill = prefer))+
+  geom_bar(stat = "count")+
+  scale_fill_manual(values = c("deepskyblue", "firebrick"))+
+  labs(title = "Preference of Female Zebrafish",
+       x = "Prefered Agressive Male",
+       y = "Number of Fish")+
+  theme(legend.position = "none")
+
+#statistical test time bb
